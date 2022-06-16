c05d3de60ce3aea356298b7eb5355de913ee012c
Erin <ebroatch@uw.edu>
2022-06-16 13:07:00 -0700

    Update roms_plots.py
    add my plot

--- plotting/roms_plots.py
+++ plotting/roms_plots.py
@@ -34,6 +34,114 @@ else: # remote linux version
     mpl.use('Agg')
 import matplotlib.pyplot as plt
     
+
+def P_sect_eb(in_dict):
+    """
+    This plots a map and a section (distance, z), and makes sure
+    that the color limits are identical.  If the color limits are
+    set automatically then the section is the preferred field for
+    setting the limits.
+    
+    I think this works best with -avl False (the default).
+    """
+    # START
+    fs = 14
+    pfun.start_plot(fs=fs, figsize=(20,9))
+    fig = plt.figure()
+    ds = xr.open_dataset(in_dict['fn'])
+    # PLOT CODE
+    vn = 'salt'
+    # GET DATA
+    G, S, T = zrfun.get_basic_info(in_dict['fn'])
+    # CREATE THE SECTION
+    # create track by hand
+    if True:
+        lon = G['lon_rho']
+        lat = G['lat_rho']
+        zdeep = -205
+        x = np.linspace(2.03821, -1, 500)
+        y = 45 * np.ones(x.shape)
+    # or read in a section (or list of sections)
+    else:
+        tracks_path = Ldir['data'] / 'section_lines'
+        tracks = ['Line_jdf_v0.p', 'Line_ps_main_v0.p']
+        zdeep = -300
+        xx = np.array([])
+        yy = np.array([])
+        for track in tracks:
+            track_fn = tracks_path / track
+            # get the track to interpolate onto
+            pdict = pickle.load(open(track_fn, 'rb'))
+            xx = np.concatenate((xx,pdict['lon_poly']))
+            yy = np.concatenate((yy,pdict['lat_poly']))
+        for ii in range(len(xx)-1):
+            x0 = xx[ii]
+            x1 = xx[ii+1]
+            y0 = yy[ii]
+            y1 = yy[ii+1]
+            nn = 20
+            if ii == 0:
+                x = np.linspace(x0, x1, nn)
+                y = np.linspace(y0,y1, nn)
+            else:
+                x = np.concatenate((x, np.linspace(x0, x1, nn)[1:]))
+                y = np.concatenate((y, np.linspace(y0, y1, nn)[1:]))
+    v2, v3, dist, idist0 = pfun.get_section(ds, vn, x, y, in_dict)
+    
+    # COLOR
+    # scaled section data
+    sf = pinfo.fac_dict[vn] * v3['sectvarf']
+    # now we use the scaled section as the preferred field for setting the
+    # color limits of both figures in the case -avl True
+    if in_dict['auto_vlims']:
+        pinfo.vlims_dict[vn] = pfun.auto_lims(sf)
+    
+    # PLOTTING
+    # map with section line
+    ax = fig.add_subplot(1, 3, 1)
+    cs = pfun.add_map_field(ax, ds, vn, pinfo.vlims_dict,
+            cmap=pinfo.cmap_dict[vn], fac=pinfo.fac_dict[vn], do_mask_edges=True)
+    # fig.colorbar(cs, ax=ax) # It is identical to that of the section
+    #pfun.add_coast(ax)
+    aaf = [-4, 4, 43, 47] # focus domain
+    ax.axis(aaf)
+    pfun.dar(ax)
+    pfun.add_info(ax, in_dict['fn'], loc='upper_right')
+    ax.set_title('Surface %s %s' % (pinfo.tstr_dict[vn],pinfo.units_dict[vn]))
+    ax.set_xlabel('Longitude')
+    ax.set_ylabel('Latitude')
+    # add section track
+    ax.plot(x, y, '-r', linewidth=2)
+    ax.plot(x[idist0], y[idist0], 'r', markersize=5, markerfacecolor='w',
+        markeredgecolor='r', markeredgewidth=2)
+    ax.set_xticks([-4, 0, 4])
+    ax.set_yticks([43, 45, 47])
+    # section
+    ax = fig.add_subplot(1, 3, (2, 3))
+    ax.plot(dist, v2['zbot'], 'k', linewidth=2)
+    ax.plot(dist, v2['zeta'], '-k', linewidth=2)
+    ax.set_xlim(dist.min(), dist.max())
+    ax.invert_xaxis()
+    ax.set_xticks([0, 40, 80, 120, 160, 200])
+    ax.set_ylim(zdeep, 5)
+    # plot section
+    svlims = pinfo.vlims_dict[vn]
+    cs = ax.pcolormesh(v3['distf'][1:-1,:], v3['zrf'][1:-1,:], sf[1:-1,:],
+                       vmin=svlims[0], vmax=svlims[1], cmap=pinfo.cmap_dict[vn])
+    fig.colorbar(cs, ax=ax)
+    ax.set_xlabel('Distance (km)')
+    ax.set_ylabel('Z (m)')
+    ax.set_title('Section %s %s' % (pinfo.tstr_dict[vn],pinfo.units_dict[vn]))
+    fig.tight_layout()
+    # FINISH
+    ds.close()
+    pfun.end_plot()
+    if len(str(in_dict['fn_out'])) > 0:
+        plt.savefig(in_dict['fn_out'])
+        plt.close()
+    else:
+        plt.show()
+
 def P_basic(in_dict):
     # START
     ds = xr.open_dataset(in_dict['fn'])
