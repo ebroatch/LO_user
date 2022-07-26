f3c49fb259c295162d18e259a584c21e2f7fa739
Erin <ebroatch@uw.edu>
2022-07-26 15:02:25 -0700

    mooring locations for sill model

---
+++ extract/moor/moor_sect_lon.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def earth_rad(lat_deg):
+    """
+    Calculate the Earth radius (m) at a latitude
+    (from http://en.wikipedia.org/wiki/Earth_radius) for oblate spheroid
+
+    INPUT: latitude in degrees
+
+    OUTPUT: Earth radius (m) at that latitute
+    """
+    a = 6378.137 * 1000; # equatorial radius (m)
+    b = 6356.7523 * 1000; # polar radius (m)
+    cl = np.cos(np.pi*lat_deg/180)
+    sl = np.sin(np.pi*lat_deg/180)
+    RE = np.sqrt(((a*a*cl)**2 + (b*b*sl)**2) / ((a*cl)**2 + (b*sl)**2))
+    return RE
+
+def ll2xy(lon, lat, lon0, lat0):
+    """
+    This converts lon, lat into meters relative to lon0, lat0.
+    It should work for lon, lat scalars or arrays.
+    NOTE: lat and lon are in degrees!!
+    """
+    R = earth_rad(lat0)
+    clat = np.cos(np.pi*lat0/180)
+    x = R * clat * np.pi * (lon - lon0) / 180
+    y = R * np.pi * (lat - lat0) / 180
+    return x, y
+
+def x2lon(x,lon0,lat0):
+    """
+    This converts a distance in meters to a longitude relative to lon0, along lat0.
+    NOTE: lat and lon are in degrees!!
+    """
+    R = earth_rad(lat0)
+    clat = np.cos(np.pi*lat0/180)
+    lon = (180 * x) / (R * clat * np.pi) + lon0
+    return lon
+
+moorloc_km = np.array([140, 122, 120, 118, 100, 82, 80, 78, 60, 42, 40, 38, 20])
+moorloc_meters = moorloc_km*1e3
+moorloc_lon = x2lon(moorloc_meters, 0, 45)
