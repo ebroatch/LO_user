28ddb4e45e75a85921101da9db3151f67fb6fc9c
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2023-10-05 23:37:07 -0700

    Update bulk_plot_layers.py
    qprism

--- extract/tef2/bulk_plot_layers.py
+++ extract/tef2/bulk_plot_layers.py
@@ -74,6 +74,7 @@ for sect_name in sect_list:
     # adjust units
     tef_df['Q_p'] = tef_df['q_p']/1000
     tef_df['Q_m'] = tef_df['q_m']/1000
+    tef_df['Q_prism'] = tef_df['qprism']/1000
                     
     # labels and colors
     # ylab_dict = {'Q': r'Transport $[10^{3}\ m^{3}s^{-1}]$',
