af15bb9a98401da68343433e3a0c98ad8999da9b
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2023-10-05 23:34:05 -0700

    Update bulk_plot_layers.py
    fix sorting plotting and update to match newer bulk_plot

--- extract/tef2/bulk_plot_layers.py
+++ extract/tef2/bulk_plot_layers.py
@@ -72,9 +72,14 @@ for sect_name in sect_list:
         
     fig = plt.figure()
     
-    ax1 = plt.subplot2grid((2,3), (0,0), colspan=2) # Qin, Qout
-    ax2 = plt.subplot2grid((2,3), (1,0), colspan=2) # Sin, Sout
-    ax3 = plt.subplot2grid((1,3), (0,2)) # map
+    # ax1 = plt.subplot2grid((2,3), (0,0), colspan=2) # Qin, Qout
+    # ax2 = plt.subplot2grid((2,3), (1,0), colspan=2) # Sin, Sout
+    # ax3 = plt.subplot2grid((1,3), (0,2)) # map
+
+    ax1 = plt.subplot2grid((3,3), (0,0), colspan=2) # Qin, Qout
+    ax2 = plt.subplot2grid((3,3), (1,0), colspan=2) # Sin, Sout
+    ax3 = plt.subplot2grid((3,3), (2,0), colspan=2) # Qin*DS and Qprism
+    axmap = plt.subplot2grid((1,3), (0,2)) # map
     
     ot = bulk['ot'] # (same as tef_df.index)
     
@@ -119,16 +124,36 @@ for sect_name in sect_list:
     # ax2.plot(bulk['ot'],sp,'or',alpha=alpha)
     # ax2.plot(bulk['ot'],sm,'ob',alpha=alpha)
 
-    ax1.scatter(otplot,qp,c=p_colors,alpha=alpha)
-    ax1.scatter(otplot,qm,c=m_colors,alpha=alpha)
+    ax1.scatter(otplot,qpsorted,c=p_colors,alpha=alpha)
+    ax1.scatter(otplot,qmsorted,c=m_colors,alpha=alpha)
     
-    ax2.scatter(otplot,sp,c=p_colors,alpha=alpha)
-    ax2.scatter(otplot,sm,c=m_colors,alpha=alpha)
+    ax2.scatter(otplot,spsorted,c=p_colors,alpha=alpha)
+    ax2.scatter(otplot,smsorted,c=m_colors,alpha=alpha)
     
     # ax2.plot(ot,tef_df['salt_p'].to_numpy(), color=p_color, linewidth=lw) #DON'T PLOT 2-LAYER
     # ax2.plot(ot,tef_df['salt_m'].to_numpy(), color=m_color, linewidth=lw)
     ax2.grid(True)
     ax2.set_ylabel(ylab_dict['salt'])
+    
+# Add Qin*DS, which requires figuring out which direction is "in".
+    # We use the direction that has the higher mean salinity.
+    sp_mean = tef_df['salt_p'].mean() # Ignores NaN's
+    sm_mean = tef_df['salt_m'].mean()
+    if sp_mean > sm_mean:
+        DS = tef_df['salt_p'].to_numpy()-tef_df['salt_m'].to_numpy()
+        Qin = tef_df['Q_p'].to_numpy()
+    elif sp_mean <= sm_mean:
+        DS = tef_df['salt_m'].to_numpy()-tef_df['salt_p'].to_numpy()
+        Qin = -tef_df['Q_m'].to_numpy()
+    ax3.plot(ot,DS*Qin, color='k', linewidth=lw)
+    ax3.grid(True)
+    ax3.set_ylabel(ylab_dict['QinDS'])
+    ax3.set_xlim(ot[0],ot[-1])
+    add_qprism(ax3)
+    if sp_mean > sm_mean:
+        ax3.text(.05,.05,r'$Q_{in}$ is positive', transform=ax3.transAxes)
+    elif sp_mean <= sm_mean:
+        ax3.text(.05,.05,r'$Q_{in}$ is negative', transform=ax3.transAxes)
     
     # map
     sn = sect_name.replace('.p','')
@@ -151,22 +176,60 @@ for sect_name in sect_list:
     elif uv1=='v':
         x1 = xv[j1,i1]
         y1 = yv[j1,i1]
-    ax3.plot([x0,x1],[y0,y1],'-c', lw=3)
-    ax3.plot(x0,y0,'og', ms=10)
-    pfun.add_coast(ax3)
-    pfun.dar(ax3)
-    ax3.pcolormesh(xp, yp, -h, vmin=-100, vmax=100,
+    axmap.plot([x0,x1],[y0,y1],'-c', lw=3)
+    axmap.plot(x0,y0,'og', ms=10)
+    #pfun.add_coast(axmap)
+    pfun.dar(axmap)
+    axmap.pcolormesh(xp, yp, -h, vmin=-200, vmax=100,
         cmap='jet', alpha=.4)
     
     dx = x1-x0; dy = y1-y0
     xmid = x0 + dx/2; ymid = y0 + dy/2
     pad = np.max((np.sqrt(dx**2 + dy**2)*2,.1))
-    ax3.axis([x0-pad, x1+pad, y0-pad, y1+pad])
-    ax3.set_xlabel('Longitude [deg]')
-    ax3.set_ylabel('Latitude [deg]')
-    ax3.set_title(sn)
-    ax3.text(xmid - np.max((dy,.05))/6, ymid + np.max((dx,.05))/6, '+', fontweight='bold', c='r', fontsize=20,
+    axmap.axis([x0-pad, x1+pad, y0-pad, y1+pad])
+    axmap.set_xlabel('Longitude [deg]')
+    axmap.set_ylabel('Latitude [deg]')
+    axmap.set_title(sn)
+    axmap.text(xmid - np.max((dy,.05))/6, ymid + np.max((dx,.05))/6, '+', fontweight='bold', c='r', fontsize=20,
         ha='center',va='center')
+
+    # # map
+    # sn = sect_name.replace('.p','')
+    # sinfo = sect_df.loc[sect_df.sn==sn,:]
+    # i0 = sinfo.iloc[0,:].i
+    # j0 = sinfo.iloc[0,:].j
+    # uv0 = sinfo.iloc[0,:].uv
+    # i1 = sinfo.iloc[-1,:].i
+    # j1 = sinfo.iloc[-1,:].j
+    # uv1 = sinfo.iloc[-1,:].uv
+    # if uv0=='u':
+    #     x0 = xu[j0,i0]
+    #     y0 = yu[j0,i0]
+    # elif uv0=='v':
+    #     x0 = xv[j0,i0]
+    #     y0 = yv[j0,i0]
+    # if uv1=='u':
+    #     x1 = xu[j1,i1]
+    #     y1 = yu[j1,i1]
+    # elif uv1=='v':
+    #     x1 = xv[j1,i1]
+    #     y1 = yv[j1,i1]
+    # ax3.plot([x0,x1],[y0,y1],'-c', lw=3)
+    # ax3.plot(x0,y0,'og', ms=10)
+    # #pfun.add_coast(ax3)
+    # pfun.dar(ax3)
+    # ax3.pcolormesh(xp, yp, -h, vmin=-100, vmax=100,
+    #     cmap='jet', alpha=.4)
+    
+    # dx = x1-x0; dy = y1-y0
+    # xmid = x0 + dx/2; ymid = y0 + dy/2
+    # pad = np.max((np.sqrt(dx**2 + dy**2)*2,.1))
+    # ax3.axis([x0-pad, x1+pad, y0-pad, y1+pad])
+    # ax3.set_xlabel('Longitude [deg]')
+    # ax3.set_ylabel('Latitude [deg]')
+    # ax3.set_title(sn)
+    # ax3.text(xmid - np.max((dy,.05))/6, ymid + np.max((dx,.05))/6, '+', fontweight='bold', c='r', fontsize=20,
+    #     ha='center',va='center')
 
     ax1.set_xlim(pd.Timestamp('2020-06-01'), pd.Timestamp('2020-07-31'))
     ax2.set_xlim(pd.Timestamp('2020-06-01'), pd.Timestamp('2020-07-31'))
