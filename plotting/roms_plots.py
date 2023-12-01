16a63db4e8d319fbeb9939c74f4935397d59300a
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2023-12-01 00:33:38 -0800

    Update roms_plots.py
    need cell centers for contour

--- plotting/roms_plots.py
+++ plotting/roms_plots.py
@@ -312,7 +312,7 @@ def P_sect_contour_eb(in_dict):
     #                    vmin=svlims[0], vmax=svlims[1], cmap=pinfo.cmap_dict[vn]) #new
     # cs = ax.contour(dist_se,zw_se,sf,
     #                    vmin=svlims[0], vmax=svlims[1], cmap=pinfo.cmap_dict[vn]) #contour
-    cs = ax.contour(dist_se,zw_se,sf,
+    cs = ax.contour((dist_se[:-1,:-1]+dist_se[1:,:-1]+dist_se[:-1,1:]+dist_se[1:,1:])/4,(zw_se[:-1,:-1]+zw_se[1:,:-1]+zw_se[:-1,1:]+zw_se[1:,1:])/4,sf,
                        vmin=20, vmax=34, cmap=pinfo.cmap_dict[vn]) #contour with manual vmax/vmin
     ax.clabel(cs, inline=True, fontsize=12)
     #fig.colorbar(cs, ax=ax)
