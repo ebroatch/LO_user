75cb947e04a9fa8b5572de90859bbc86f94f6a2b
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2024-02-07 15:05:34 -0800

    Update create_sections_user.py
    add new sections for new grids

--- extract/tef2/create_sections_user.py
+++ extract/tef2/create_sections_user.py
@@ -29,15 +29,61 @@ else:
     # make out_dir in case it does not exist yet
     Lfun.make_dir(out_dir)
 
-sn_list = ['a1','a2','a3','a4','a5','b1','b2','b3','b4','b5','c1','c2','c3','c4','c5']
-x1km=np.array([0,10,20,30,38,40,42,44,46,48,50,58,68,78,86])
-x1m=x1km*1e3
-x1=llxyfun.x2lon(x1m,0,45)
-y1=45.05
-y2=44.95
-for i in range(len(sn_list)):
-    sn=sn_list[i]
-    df = pd.DataFrame(columns=['x','y'])
-    df.loc[:,'x'] = np.array([x1[i], x1[i]])
-    df.loc[:,'y'] = np.array([y1, y2])
-    df.to_pickle(out_dir / (sn + '.p'))
+if args.gridname == 'sill3':
+    sn_list = ['a1','a2','a3','a4','a5','b1','b2','b3','b4','b5','c1','c2','c3','c4','c5']
+    x1km=np.array([0,10,20,30,38,40,42,44,46,48,50,58,68,78,86])
+    x1m=x1km*1e3
+    x1=llxyfun.x2lon(x1m,0,45)
+    y1=45.05
+    y2=44.95
+    for i in range(len(sn_list)):
+        sn=sn_list[i]
+        df = pd.DataFrame(columns=['x','y'])
+        df.loc[:,'x'] = np.array([x1[i], x1[i]])
+        df.loc[:,'y'] = np.array([y1, y2])
+        df.to_pickle(out_dir / (sn + '.p'))
+
+elif args.gridname == 'sill5km':
+    sn_list = ['a1','a2','a3','a4','a5','b1','b2','b3','b4','b5','c1','c2','c3','c4','c5']
+    x1km=np.array([0,10,20,30,38,40,41.25,42.5,43.75,45,47,55,65,75,83])
+    x1m=x1km*1e3
+    x1=llxyfun.x2lon(x1m,0,45)
+    y1=45.05
+    y2=44.95
+    for i in range(len(sn_list)):
+        sn=sn_list[i]
+        df = pd.DataFrame(columns=['x','y'])
+        df.loc[:,'x'] = np.array([x1[i], x1[i]])
+        df.loc[:,'y'] = np.array([y1, y2])
+        df.to_pickle(out_dir / (sn + '.p'))
+
+elif args.gridname == 'sill20kmdeep':
+    sn_list = ['a1','a2','a3','a4','a5','b1','b2','b3','b4','b5','c1','c2','c3','c4','c5']
+    x1km=np.array([0,10,20,30,38,40,45,50,55,60,62,70,80,90,98])
+    x1m=x1km*1e3
+    x1=llxyfun.x2lon(x1m,0,45)
+    y1=45.05
+    y2=44.95
+    for i in range(len(sn_list)):
+        sn=sn_list[i]
+        df = pd.DataFrame(columns=['x','y'])
+        df.loc[:,'x'] = np.array([x1[i], x1[i]])
+        df.loc[:,'y'] = np.array([y1, y2])
+        df.to_pickle(out_dir / (sn + '.p'))
+
+elif args.gridname == 'sill80km':
+    sn_list = ['a1','a2','a3','a4','a5','b1','b2','b3','b4','b5','c1','c2','c3','c4','c5']
+    x1km=np.array([0,10,20,30,38,40,60,80,100,120,122,130,140,150,158])
+    x1m=x1km*1e3
+    x1=llxyfun.x2lon(x1m,0,45)
+    y1=45.05
+    y2=44.95
+    for i in range(len(sn_list)):
+        sn=sn_list[i]
+        df = pd.DataFrame(columns=['x','y'])
+        df.loc[:,'x'] = np.array([x1[i], x1[i]])
+        df.loc[:,'y'] = np.array([y1, y2])
+        df.to_pickle(out_dir / (sn + '.p'))
+
+else:
+    print('Error: unsupported gridname')
