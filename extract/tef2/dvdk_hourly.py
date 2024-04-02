9c0bdb51448de859a4d7a0c7965a22364b2372f7
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2024-04-02 16:32:39 -0700

    Update dvdk_hourly.py
    fix typos and remove extra calcs leftover from standard decomp

--- extract/tef2/dvdk_hourly.py
+++ extract/tef2/dvdk_hourly.py
@@ -137,7 +137,7 @@ for ext_fn in sect_list:
     dvdk['Q']=(Q[pad:-pad+1])[pad:-pad+1]
     dvdk['Q0']=u0[pad:-pad+1]
     dvdk['Q1']=u1[pad:-pad+1]
-    dvdk['F0']=FR[pad:-pad+1]
+    dvdk['F0']=F0[pad:-pad+1]
     dvdk['F1']=F1
     dvdk['F2']=F2
     dvdk['F2L']=F2L
@@ -150,9 +150,9 @@ for ext_fn in sect_list:
     #Saving as .nc dataset based on bulk_calc
     sdlist=['A','sbar','s0','s1','ubar','u0','u1','Q','Q0','Q1','F0','F1','F2','F2L','F2V','F','ssh','ssh_lp']
     # Pack results in a Dataset and then save to NetCDF
-    ds = xr.Dataset(coords={'time': SD['ot']})
+    ds = xr.Dataset(coords={'time': dvdk['ot']})
     for vn in sdlist:
-        ds[vn] = (('time'), SD[vn])
+        ds[vn] = (('time'), dvdk[vn])
     # save it to NetCDF
     ds.to_netcdf(out_dir / out_fn)
     if Ldir['testing']:
@@ -161,150 +161,6 @@ for ext_fn in sect_list:
         ds.close()
     print('  elapsed time for section = %d seconds' % (time()-tt0))
     sys.stdout.flush()
-
-    ############
-
-    A0 = zfun.lowpass(A, f='godin')[pad:-pad+1] #shape NT-72
-    dA0 = zfun.lowpass(dA, f='godin')[pad:-pad+1, :, :] #shape NT-72,NZ,NX
-
-    u0 = (zfun.lowpass(np.sum(q, axis=(1,2)), f='godin')[pad:-pad+1])/A0 #shape NT-72
-    s0 = (zfun.lowpass(np.sum(sdA, axis=(1,2)), f='godin')[pad:-pad+1])/A0 #shape NT-72
-
-    u1 = (zfun.lowpass(q, f='godin')[pad:-pad+1, :, :])/dA0 - u0[:, np.newaxis, np.newaxis] #shape NT-72,NZ,NX newaxis for broadcasting
-    s1 = (zfun.lowpass(sdA, f='godin')[pad:-pad+1, :, :])/dA0 - s0[:, np.newaxis, np.newaxis] #shape NT-72,NZ,NX newaxis for broadcasting
-
-    u2 = u[pad:-pad+1, :, :] - u1 - u0[:, np.newaxis, np.newaxis] #shape NT-72,NZ,NX
-    s2 = s[pad:-pad+1, :, :] - s1 - s0[:, np.newaxis, np.newaxis] #shape NT-72,NZ,NX
-    dA2 = dA[pad:-pad+1, :, :] #shape NT-72,NZ,NX
-
-    u2L = (np.sum(u2*dz[pad:-pad+1, :, :],axis=1))/(H[pad:-pad+1, :]) #shape NT-72,NX
-    u2V = u2 - np.expand_dims(u2L,axis=1) #shape NT-72,NZ,NX expand_dims for broadcasting
-    s2L = (np.sum(s2*dz[pad:-pad+1, :, :],axis=1))/(H[pad:-pad+1, :]) #shape NT-72,NX
-    s2V = s2 - np.expand_dims(s2L,axis=1) #shape NT-72,NZ,NX expand_dims for broadcasting
-
-    ssh = np.mean(zeta, axis=1)[pad:-pad+1] #shape NT-72
-    ssh_lp = zfun.lowpass(np.mean(zeta, axis=1), f='godin')[pad:-pad+1] #shape NT-72
-    
-    FR = (u0*s0*A0) #shape NT-72
-    FE = np.sum(u1*s1*dA0, axis=(1,2)) #shape NT-72
-    FT = zfun.lowpass(np.sum(u2*s2*dA2, axis=(1,2)), f='godin')[pad:-pad+1] #shape NT-144
-
-    FTL = zfun.lowpass(np.sum(u2L*s2L*H[pad:-pad+1, :]*dd, axis=1), f='godin')[pad:-pad+1] #shape NT-144
-    FTV = zfun.lowpass(np.sum(u2V*s2V*dA[pad:-pad+1, :, :], axis=(1,2)), f='godin')[pad:-pad+1] #shape NT-144
-
-    F = zfun.lowpass(np.sum(u*s*dA, axis=(1,2)), f='godin')[pad:-pad+1] #shape NT-72
-
-    SD = dict()
-    SD['u0']=u0[pad:-pad+1]
-    SD['s0']=s0[pad:-pad+1]
-    SD['A0']=A0[pad:-pad+1]
-    SD['FR']=FR[pad:-pad+1]
-    SD['FE']=FE[pad:-pad+1]
-    SD['FT']=FT
-    SD['FTL']=FTL
-    SD['FTV']=FTV
-    SD['F']=F[pad:-pad+1]
-    SD['ssh']=ssh[pad:-pad+1] #fix typo
-    SD['ssh_lp']=ssh_lp[pad:-pad+1]
-    SD['ot']=(ot[pad:-pad+1])[pad:-pad+1]
-    pickle.dump(SD, open(out_dir / out_fn, 'wb'))
-    print('  elapsed time for section = %d seconds' % (time()-tt0))
-    sys.stdout.flush()
-
-    # # make arrays of property transport
-    # QV = dict()
-    # for vn in V.keys():
-    #     if vn == 'q':
-    #         QV[vn] = q
-    #     else:
-    #         QV[vn] = q*V[vn]
-    
-    
-    # # define salinity bins
-    # if Ldir['testing']:
-    #     NS = 36 # number of salinity bins
-    # else:
-    #     NS = 1000 # number of salinity bins
-    # S_low = 0
-    # S_hi = 36
-    # sedges = np.linspace(S_low, S_hi, NS+1)
-    # sbins = sedges[:-1] + np.diff(sedges)/2
-
-    # # TEF variables
-    # Omat = np.zeros((NT, NS))
-    # TEF = dict()
-    # for vn in QV.keys():
-    #     TEF[vn] = Omat.copy()
-    # # other variables
-    # omat = np.zeros(NT)
-    # qnet = omat.copy()
-    # fnet = omat.copy()
-    # ssh = omat.copy()
-    # g = 9.8
-    # rho = 1025
-
-    # # Process into salinity bins.
-    # # NOTE: this seems like a lot of nested loops (section/time/variable/digitization indices)
-    # # but it runs reasonably fast. Also we only have to run it once.
-    # for tt in range(NT):
-            
-    #     if False:
-    #         sf = V['salt'][tt,:,:].squeeze().flatten()
-    #         # sort into salinity bins
-    #         inds = np.digitize(sf, sedges, right=True)
-    #         indsf = inds.copy().flatten()
-            
-    #         for vn in QV.keys():
-    #             XI = QV[vn][tt,:,:].squeeze()
-    #             XF = zfun.fillit(XI).flatten()
-    #             XF = XF[~np.isnan(XF)]
-    #             if vn == 'q':
-    #                 # also keep track of volume transport
-    #                 qnet[tt] = XF.sum()
-    #                 # and tidal energy flux
-    #                 zi = zeta[tt,:].squeeze()
-    #                 ssh[tt] = zi.mean()
-    #                 fnet[tt] = g * rho * ssh[tt] * qnet[tt]
-    #             counter = 0
-    #             for ii in indsf:
-    #                 TEF[vn][tt, ii-1] += XF[counter]
-    #                 counter += 1
-                
-                
-    #             if Ldir['testing'] and (tt==10) and (vn=='salt'):
-    #                 print(TEF[vn][tt,:])
-                
-    #     else:
-    #         # alternate version
-            
-    #         sf = V['salt'][tt,:,:].squeeze().flatten()
-            
-    #         for vn in QV.keys():
-    #             XF = QV[vn][tt,:,:].squeeze().flatten()
-    #             if vn == 'q':
-    #                 # also keep track of volume transport
-    #                 qnet[tt] = XF.sum()
-    #                 # and tidal energy flux
-    #                 zi = zeta[tt,:].squeeze()
-    #                 ssh[tt] = zi.mean()
-    #                 fnet[tt] = g * rho * ssh[tt] * qnet[tt]
-                    
-    #             # scipy.stats.binned_statistic(x, values, statistic='mean', bins=10, range=None)
-    #             TEF[vn][tt,:] = binned_statistic(sf, XF, statistic='sum', bins=NS, range=(S_low,S_hi)).statistic
-                
-    #             # results are identical
-                
-                
-    #             if Ldir['testing'] and (tt==10) and (vn=='salt'):
-    #                 print(TEF[vn][tt,:])
-            
-        
-    
-    # TEF['ot'] = ot
-    # TEF['sbins'] = sbins
-    # TEF['qnet'] = qnet
-    # TEF['fnet'] = fnet
-    # TEF['ssh'] = ssh
 
 print('\nTotal elapsed time = %d seconds' % (time()-tt00))
 
