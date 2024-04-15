628f65d94e1668d44f696d325743c6ff5021bf58
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2024-04-15 01:00:40 -0700

    Update roms_plots.py

--- plotting/roms_plots.py
+++ plotting/roms_plots.py
@@ -313,10 +313,10 @@ def P_sect_contour_eb(in_dict):
     # cs = ax.contour(dist_se,zw_se,sf,
     #                    vmin=svlims[0], vmax=svlims[1], cmap=pinfo.cmap_dict[vn]) #contour
     
-    # cs = ax.contour((dist_se[:-1,:-1]+dist_se[1:,:-1]+dist_se[:-1,1:]+dist_se[1:,1:])/4,(zw_se[:-1,:-1]+zw_se[1:,:-1]+zw_se[:-1,1:]+zw_se[1:,1:])/4,sf,
-    #                    vmin=22, vmax=34, levels=25, cmap=pinfo.cmap_dict[vn]) #contour with manual vmax/vmin
-    cs = ax.contourf((dist_se[:-1,:-1]+dist_se[1:,:-1]+dist_se[:-1,1:]+dist_se[1:,1:])/4,(zw_se[:-1,:-1]+zw_se[1:,:-1]+zw_se[:-1,1:]+zw_se[1:,1:])/4,sf,
-                       vmin=22, vmax=34, levels=25, cmap=pinfo.cmap_dict[vn]) #contour with manual vmax/vmin
+    cs = ax.contour((dist_se[:-1,:-1]+dist_se[1:,:-1]+dist_se[:-1,1:]+dist_se[1:,1:])/4,(zw_se[:-1,:-1]+zw_se[1:,:-1]+zw_se[:-1,1:]+zw_se[1:,1:])/4,sf,
+                        vmin=22, vmax=34, levels=25, colors='k')#, cmap=pinfo.cmap_dict[vn]) #contour with manual vmax/vmin
+    #cs = ax.contourf((dist_se[:-1,:-1]+dist_se[1:,:-1]+dist_se[:-1,1:]+dist_se[1:,1:])/4,(zw_se[:-1,:-1]+zw_se[1:,:-1]+zw_se[:-1,1:]+zw_se[1:,1:])/4,sf,
+    #                   vmin=22, vmax=34, levels=25, cmap=pinfo.cmap_dict[vn]) #contour with manual vmax/vmin
     
     ax.clabel(cs, inline=True, fontsize=12)
     #fig.colorbar(cs, ax=ax)
