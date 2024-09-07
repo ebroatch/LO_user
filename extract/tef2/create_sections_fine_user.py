1ced6d82aea245a718b9d9254d996a716ec92d78
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2024-09-06 18:17:25 -0700

    Update create_sections_fine_user.py

--- extract/tef2/create_sections_fine_user.py
+++ extract/tef2/create_sections_fine_user.py
@@ -62,7 +62,7 @@ if args.gridname == 'sill20kmdeep':
         df = pd.DataFrame(columns=['x','y'])
         df.loc[:,'x'] = np.array([x1[i], x1[i]])
         df.loc[:,'y'] = np.array([y1, y2])
-        # df.to_pickle(out_dir / (sn + '.p')) #uncomment when ready
+        df.to_pickle(out_dir / (sn + '.p'))
 
 # elif args.gridname == 'sill3':
 #     sn_list = ['a1','a2','a3','a4','a5','b1','b2','b3','b4','b5','c1','c2','c3','c4','c5']
