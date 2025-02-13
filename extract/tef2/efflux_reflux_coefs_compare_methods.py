845e1782998da5b15b42e91b78aefc45b42bf19d
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2025-02-13 00:18:56 -0800

    Update efflux_reflux_coefs_compare_methods.py

--- extract/tef2/efflux_reflux_coefs_compare_methods.py
+++ extract/tef2/efflux_reflux_coefs_compare_methods.py
@@ -350,18 +350,18 @@ print('got alphas from tidally averaged particles')
 ########## PLOTTING ##########
 fig, ax = plt.subplots(1,1,figsize=(15,8))
 
-ax.plot(silllens_plot,alpha_34_adj,marker='o',c='tab:pink',ls='-',label='Adjusted TEF')
-ax.plot(silllens_plot,alpha_21_adj,marker='o',c='tab:cyan',ls='-',label='Adjusted TEF')
-ax.plot(silllens_plot,alpha_24_adj,marker='o',c=plt.cm.tab20(13),ls='-',label='Adjusted TEF')
-ax.plot(silllens_plot,alpha_31_adj,marker='o',c=plt.cm.tab20(19),ls='-',label='Adjusted TEF')
-ax.plot(silllens_plot,alpha_34_par_ta,marker='o',c='tab:pink',ls='--',label='Tidally averaged particles')
-ax.plot(silllens_plot,alpha_21_par_ta,marker='o',c='tab:cyan',ls='--',label='Tidally averaged particles')
-ax.plot(silllens_plot,alpha_24_par_ta,marker='o',c=plt.cm.tab20(13),ls='--',label='Tidally averaged particles')
-ax.plot(silllens_plot,alpha_31_par_ta,marker='o',c=plt.cm.tab20(19),ls='--',label='Tidally averaged particles')
-ax.plot(silllens_plot,alpha_34_par,marker='o',c='tab:pink',ls=':',label='Particles')
-ax.plot(silllens_plot,alpha_21_par,marker='o',c='tab:cyan',ls=':',label='Particles')
-ax.plot(silllens_plot,alpha_24_par,marker='o',c=plt.cm.tab20(13),ls=':',label='Particles')
-ax.plot(silllens_plot,alpha_31_par,marker='o',c=plt.cm.tab20(19),ls=':',label='Particles')
+ax.plot(silllens_plot,alpha_34_adj,marker='o',c='tab:pink',ls='-',label=r'Adjusted TEF $\alpha_{34}$')
+ax.plot(silllens_plot,alpha_21_adj,marker='o',c='tab:cyan',ls='-',label=r'Adjusted TEF $\alpha_{21}$')
+ax.plot(silllens_plot,alpha_24_adj,marker='o',c=plt.cm.tab20(13),ls='-',label=r'Adjusted TEF $\alpha_{24}$')
+ax.plot(silllens_plot,alpha_31_adj,marker='o',c=plt.cm.tab20(19),ls='-',label=r'Adjusted TEF $\alpha_{31}$')
+ax.plot(silllens_plot,alpha_34_par_ta,marker='o',c='tab:pink',ls='--',label=r'Tidally averaged particles $\alpha_{34}$')
+ax.plot(silllens_plot,alpha_21_par_ta,marker='o',c='tab:cyan',ls='--',label=r'Tidally averaged particles $\alpha_{21}$')
+ax.plot(silllens_plot,alpha_24_par_ta,marker='o',c=plt.cm.tab20(13),ls='--',label=r'Tidally averaged particles $\alpha_{24}$')
+ax.plot(silllens_plot,alpha_31_par_ta,marker='o',c=plt.cm.tab20(19),ls='--',label=r'Tidally averaged particles $\alpha_{31}$')
+ax.plot(silllens_plot,alpha_34_par,marker='o',c='tab:pink',ls=':',label=r'Particles $\alpha_{34}$')
+ax.plot(silllens_plot,alpha_21_par,marker='o',c='tab:cyan',ls=':',label=r'Particles $\alpha_{21}$')
+ax.plot(silllens_plot,alpha_24_par,marker='o',c=plt.cm.tab20(13),ls=':',label=r'Particles $\alpha_{24}$')
+ax.plot(silllens_plot,alpha_31_par,marker='o',c=plt.cm.tab20(19),ls=':',label=r'Particles $\alpha_{31}$')
 
 ax.set_xlabel('Sill length [km]')
 ax.set_ylabel('Efflux/reflux coefficients')
@@ -373,7 +373,7 @@ ax.grid(True)
 h, l = ax.get_legend_handles_labels()
 ph = [plt.plot([],marker="", ls="")[0]]*4
 handles = ph + h
-labels = [r'Inner basin reflux $\alpha_{34}$:', r'Efflux from inner basin $\alpha_{24}$',r'Efflux from inner basin $\alpha_{24}$',r'Efflux from outer basin $\alpha_{31}$'] + l
+labels = [r'Inner basin reflux $\alpha_{34}$:', r'Outer basin reflux $\alpha_{21}$:',r'Efflux from inner basin $\alpha_{24}$:',r'Efflux from outer basin $\alpha_{31}$:'] + l
 ax.legend(handles, labels, ncol=4)
 
 fn_fig = Ldir['LOo'] / 'plots' / 'efflux_reflux_coefs_compare_methods.png' #UNCOMMENT TO PLOT
