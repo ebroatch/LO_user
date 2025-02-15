428dff5407e9681b847881c95007ed283792ca65
Erin Broatch <45082650+ebroatch@users.noreply.github.com>
2025-02-14 23:41:13 -0800

    Update efflux_reflux_coefs_time_dependent.py
    add bottom layer calculation

--- extract/tef2/efflux_reflux_coefs_time_dependent.py
+++ extract/tef2/efflux_reflux_coefs_time_dependent.py
@@ -77,6 +77,11 @@ out_dir0 = Ldir['LOo'] / 'extract' / Ldir['gtagex'] / 'tef2'
 
 fig, ax = plt.subplots(1,1,figsize=(15,8))
 
+# take a subset of the data so that we are averaging over an integer number of spring neap cycles at the end
+# use 7 spring neap cycles, starting at index 257 and going to 2741 - these are the peaks in the 5km Qprism but similar for the other models
+start_avg_ind = 257
+end_avg_ind = 2741
+
 #Loop over sill lengths
 for i in range(len(gctags)):
 # for i in range(len(gctags)-1):
@@ -96,10 +101,10 @@ for i in range(len(gctags)):
     # tef_df['Q_m'] = tef_df['q_m']/1000
     # tef_df['Q_prism']=tef_df['qprism']/1000
     # get variables for efflux-reflux calculation
-    Q1 = tef_df['q_p'] #keep original units for comparing with V_sill
-    Q2 = -tef_df['q_m']
-    S1 = tef_df['salt_p']
-    S2 = tef_df['salt_m']
+    Q1 = tef_df['q_p'][start_avg_ind:end_avg_ind] #keep original units for comparing with V_sill #maybe should add .values???
+    Q2 = -tef_df['q_m'][start_avg_ind:end_avg_ind]
+    S1 = tef_df['salt_p'][start_avg_ind:end_avg_ind]
+    S2 = tef_df['salt_m'][start_avg_ind:end_avg_ind]
 
     #Section b5
     sect_name = sect_2
@@ -110,10 +115,10 @@ for i in range(len(gctags)):
     # tef_df['Q_m'] = tef_df['q_m']/1000
     # tef_df['Q_prism']=tef_df['qprism']/1000
     # get variables for efflux-reflux calculation
-    Q3 = tef_df['q_p'] #keep original units for comparing with V_sill
-    Q4 = -tef_df['q_m']
-    S3 = tef_df['salt_p']
-    S4 = tef_df['salt_m']
+    Q3 = tef_df['q_p'][start_avg_ind:end_avg_ind] #keep original units for comparing with V_sill
+    Q4 = -tef_df['q_m'][start_avg_ind:end_avg_ind]
+    S3 = tef_df['salt_p'][start_avg_ind:end_avg_ind]
+    S4 = tef_df['salt_m'][start_avg_ind:end_avg_ind]
 
     #Section b3 - we are using this to get an estimate of salinities in the upper and lower layers on the sill
     sect_name = sect_mid
@@ -124,8 +129,8 @@ for i in range(len(gctags)):
     # tef_df['Q_m'] = tef_df['q_m']/1000
     # tef_df['Q_prism']=tef_df['qprism']/1000
     # get variables for storage term in efflux-reflux calculation
-    S_bottom = tef_df['salt_p']
-    S_top = tef_df['salt_m']
+    S_bottom = tef_df['salt_p'][start_avg_ind-1:end_avg_ind+1] #keep one extra value on each side for the salt
+    S_top = tef_df['salt_m'][start_avg_ind-1:end_avg_ind+1] #this way when we do centered differences the length will be the same as the other variables
 
     #Get the volume of the sill area
     grid_dir = Ldir['data'] / 'grids' / gridname
@@ -170,36 +175,53 @@ for i in range(len(gctags)):
     # vol_residual = Q1-Q2-Q3+Q4
     # salt_residual = (Q1*S1)-(Q2*S2)-(Q3*S3)+(Q4*S4)
 
-    #calculate alphas basic without adjustment or storage term
+    #calculate alphas basic from timeseries (not means) without adjustment or storage term
     alpha_21_basic_timeseries = (Q2/Q1)*((S2-S4)/(S1-S4))
     alpha_31_basic_timeseries = (Q3/Q1)*((S3-S4)/(S1-S4))
     alpha_24_basic_timeseries = (Q2/Q4)*((S1-S2)/(S1-S4))
     alpha_34_basic_timeseries = (Q3/Q4)*((S1-S3)/(S1-S4))
 
-    print('outer reflux w/o storage')
-    print(alpha_21_basic_timeseries.mean())
-    print('inner reflux w/o storage')
-    print(alpha_34_basic_timeseries.mean())
-
     #calculate storage term with centered differences #make sure to use 3600s and volume divided by 1000
     ddt_S_top = (S_top.values[2:]-S_top.values[:-2])/(2*3600)
-    ddt_S_top = np.concatenate(([np.nan],ddt_S_top,[np.nan]))
-    storage_21 = (1/(Q1*(S1-S4)))*V_top*ddt_S_top
-    storage_24 = (1/(Q4*(S4-S1)))*V_top*ddt_S_top
+    # ddt_S_top = np.concatenate(([np.nan],ddt_S_top,[np.nan]))
+    storage_21_top = (1/(Q1*(S1-S4)))*V_top*ddt_S_top
+    storage_24_top = (1/(Q4*(S4-S1)))*V_top*ddt_S_top
 
     #calculate time dependent alphas
-    alpha_21_td = alpha_21_basic_timeseries + storage_21
-    alpha_24_td = alpha_24_basic_timeseries + storage_24
-    alpha_34_td = 1-alpha_24_td
+    alpha_21_td_top = alpha_21_basic_timeseries + storage_21_top
+    alpha_24_td_top = alpha_24_basic_timeseries + storage_24_top
+    alpha_31_td_top = 1-alpha_21_td_top
+    alpha_34_td_top = 1-alpha_24_td_top
 
-    print('outer reflux w/ storage')
-    print(alpha_21_td.mean())
-    print('inner reflux w/ storage')
-    print(alpha_34_td.mean())
+
+    #to test the method, try calculating the same terms from the bottom layer budget
+    ddt_S_bottom = (S_bottom.values[2:]-S_bottom.values[:-2])/(2*3600)
+    storage_31_bottom = (1/(Q1*(S1-S4)))*V_bottom*ddt_S_bottom    
+    storage_34_bottom = (1/(Q4*(S4-S1)))*V_bottom*ddt_S_bottom
+
+    alpha_31_td_bottom = alpha_31_basic_timeseries + storage_31_bottom
+    alpha_34_td_bottom = alpha_34_basic_timeseries + storage_34_bottom
+    alpha_21_td_bottom = 1-alpha_31_td_bottom
+    alpha_24_td_bottom = 1-alpha_34_td_bottom
+
+
+    #print some values
+    print('outer reflux w/o storage')
+    print(alpha_21_basic_timeseries.mean())
+    print('outer reflux w/ storage (top layer calc)')
+    print(alpha_21_td_top.mean())
+    print('outer reflux w/ storage (bottom layer calc)')
+    print(alpha_21_td_bottom.mean())
+    print('inner reflux w/o storage')
+    print(alpha_34_basic_timeseries.mean())
+    print('inner reflux w/ storage (top layer calc)')
+    print(alpha_34_td_top.mean())
+    print('inner reflux w/ storage (bottom layer calc)')
+    print(alpha_34_td_bottom.mean())
 
     #plot
-    ax.plot(tef_df.index,alpha_34_td,ls='-',c=plot_color[i],label=r'$\alpha_{34}$ '+silllens[i])
-    ax.plot(tef_df.index,alpha_21_td,ls='--',c=plot_color[i],label=r'$\alpha_{21}$ '+silllens[i])
+    ax.plot(tef_df.index,alpha_34_td_top,ls='-',c=plot_color[i],label=r'$\alpha_{34}$ '+silllens[i])
+    ax.plot(tef_df.index,alpha_21_td_top,ls='--',c=plot_color[i],label=r'$\alpha_{21}$ '+silllens[i])
 
 #add plot elements
 ax.set_xlabel('Time')
